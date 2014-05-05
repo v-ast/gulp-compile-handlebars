dd1b6bee81223fb02026be95f017bd1cb63a854b
Florian <accounts@flovan.me>
2014-05-05 23:15:36 +0200

    Fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var Handlebars = require('handlebars');
+var fs = require('fs');
 
 module.exports = function (data, opts) {
 
@@ -21,13 +22,20 @@ module.exports = function (data, opts) {
 	// Go through a partials directory array
 	if(options.batch){
 		// Allow single string
-		if(typeof options.batch === 'string') {
-			Handlebars.registerPartials(options.batch);
-		} else {
-			for(var i = 0, l = options.batch.length, b = options.batch[i]; i < l; i++) {
-				Handlebars.registerPartials(b);
-			}
-		}
+		if(typeof options.batch === 'string') options.batch = [options.batch];
+			
+		options.batch.forEach(function (b) {
+			var filenames = fs.readdirSync(b);
+
+			filenames.forEach(function (filename) {
+				// Needs a better name extractor (maybe with the path module)
+				var name = filename.split('.')[0];
+				// Don't allow hidden files
+				if(!name.length) return;
+				var template = fs.readFileSync(b + '/' + filename, 'utf8');
+				Handlebars.registerPartial(b.split('/').pop() + '/' + name, template);
+			});
+		});
 	}
 
 
